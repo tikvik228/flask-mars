364e9a782bb1db9e80d26c3a28f7e812d2ecef98
eugennne <tikvik228@mail.ru>
2025-02-20 18:15:10 +0300

    engineer and science commit

--- second_file.py
+++ second_file.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index(title):
     return render_template('base.html', title=title)
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof.lower())
+
 if __name__ == "__main__":
     app.run(port=8080, host='127.0.0.1')
