bc625a835f210ea7932439ccacf151045875dede
eugennne <tikvik228@mail.ru>
2025-02-24 17:39:27 +0300

    jobs list commit

--- second_file.py
+++ second_file.py
@@ -16,5 +16,20 @@ def list_prof(type):
     jobs = ["пилот", "врач", "ученый", "штурман"]
     return render_template('jobs_list.html', type=type.lower(), professions=jobs)
 
+@app.route('/auto_answer')
+@app.route('/answer')
+def auto_answer():
+    profile = {}
+    profile["title"] = "Ваша анкета миссии"
+    profile["surname"] = "Борозинец"
+    profile["name"] = "София"
+    profile["education"] = "-"
+    profile["profession"] = "-"
+    profile["sex"] = "ж"
+    profile["motivation"] = "-"
+    profile["ready"] = "--"
+    return render_template("auto_answer.html", **profile)
+
+
 if __name__ == "__main__":
     app.run(port=8080, host='127.0.0.1')
