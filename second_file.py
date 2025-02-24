be18bd9784b3c2539be8a840e9872f7bcde448da
eugennne <tikvik228@mail.ru>
2025-02-24 18:46:45 +0300

    astro_commit

--- second_file.py
+++ second_file.py
@@ -54,5 +54,10 @@ def login():
 def success():
     return render_template('success.html')
 
+@app.route('/distribution')
+def distribution():
+    return render_template('distribution.html', astro_list=["Ридли Скотт", "Марк Уотни", "Энди Уир"])
+
+
 if __name__ == "__main__":
     app.run(port=8080, host='127.0.0.1')
