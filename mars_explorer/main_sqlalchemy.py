e9e7cee6979dd1aca6b97b53a79ed059537cd26e
evgen <tikvik228@mail.ru>
2025-03-01 22:20:24 +0300

    crew update

--- mars_explorer/main_sqlalchemy.py
+++ mars_explorer/main_sqlalchemy.py
@@ -8,23 +8,31 @@ app.config['SECRET_KEY'] = 'ytrewq'
 
 def main():
     db_session.global_init("db/mars_explorer.db")
-
-
     session = db_session.create_session()
-    user = User()
-    user.surname = "Scott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = "captain"
-    user.speciality = "research engineer"
-    user.address = "module_1"
-    user.email = "scott_chief@mars.org"
-    user.hashed_password = "cap"
-    user.set_password(user.hashed_password)
-    session.add(user)
-    session.commit()
 
-    app.run()
+    info = {"surname": ["Scott", "Watney", "Lewis", "Vogel"],
+            "name": ["Ridley", "Mark", "Melissa", "Alex"],
+            "age": [21, 31, 34, 42],
+            "position": ["captain", "crew", "commander", "navigator"],
+            "speciality": ["research engineer", "botanist", "Submarine Warfare officer, oceanographer", "chemist"],
+            "address": ["module_1", "module_4", "module_1", "module_2"],
+            "email": ["scott_chief@mars.org", "potato220@mars.org", "aresIII@mars.org", "hardcore_mode@mars.org"],
+            "hashed_password": ["cap", "potato", "disco_style", "Magnesium-based_Lifeform"]}
+
+    for i in range(len(info["surname"])):
+        user = User()
+        user.surname = info["surname"][i]
+        user.name = info["name"][i]
+        user.age = info["age"][i]
+        user.position = info["position"][i]
+        user.speciality = info["speciality"][i]
+        user.address = info["address"][i]
+        user.email = info["email"][i]
+        user.hashed_password = info["hashed_password"][i]
+        user.set_password(user.hashed_password)
+        session.add(user)
+
+    session.commit()
 
 if __name__ == "__main__":
     main()
