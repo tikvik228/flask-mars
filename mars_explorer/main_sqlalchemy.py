6c7d91e7947f090fdd5681af10d44d80ca2f99af
evgen <tikvik228@mail.ru>
2025-03-06 16:33:35 +0300

    job update

--- mars_explorer/main_sqlalchemy.py
+++ mars_explorer/main_sqlalchemy.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ytrewq'
@@ -9,8 +10,16 @@ app.config['SECRET_KEY'] = 'ytrewq'
 def main():
     db_session.global_init("db/mars_explorer.db")
     session = db_session.create_session()
+    new_job = Jobs()
+    new_job.teamleader = 1
+    new_job.job = "deployment of residential modules 1 and 2"
+    new_job.work_size = 15
+    new_job.collaborators = "2, 3"
+    new_job.is_finished = False
+    session.add(new_job)
+    session.commit()
 
-    info = {"surname": ["Scott", "Watney", "Lewis", "Vogel"],
+    '''info = {"surname": ["Scott", "Watney", "Lewis", "Vogel"],
             "name": ["Ridley", "Mark", "Melissa", "Alex"],
             "age": [21, 31, 34, 42],
             "position": ["captain", "crew", "commander", "navigator"],
@@ -30,9 +39,7 @@ def main():
         user.email = info["email"][i]
         user.hashed_password = info["hashed_password"][i]
         user.set_password(user.hashed_password)
-        session.add(user)
-
-    session.commit()
+        session.add(user)'''
 
 if __name__ == "__main__":
     main()
